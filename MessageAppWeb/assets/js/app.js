9bb9b28cf05a21df265846d426e8078c66f2c496
Soroush <hatpick@gmail.com>
2015-12-23 23:26:41 -0800

    timeago

--- MessageAppWeb/assets/js/app.js
+++ MessageAppWeb/assets/js/app.js
@@ -25,6 +25,60 @@ function Message(msg) {
 	this.updated = msg.updated;
 }
 
+function timeAgo(time) {
+    switch (typeof time) {
+        case 'number':
+            break;
+        case 'string':
+            time = +new Date(time);
+            break;
+        case 'object':
+            if (time.constructor === Date) time = time.getTime();
+            break;
+        default:
+            time = +new Date();
+    }
+    var time_formats = [
+        [60, 'seconds', 1],
+        [120, '1 minute ago', '1 minute from now'],
+        [3600, 'minutes', 60],
+        [7200, '1 hour ago', '1 hour from now'],
+        [86400, 'hours', 3600],
+        [172800, 'Yesterday', 'Tomorrow'],
+        [604800, 'days', 86400],
+        [1209600, 'Last week', 'Next week'],
+        [2419200, 'weeks', 604800],
+        [4838400, 'Last month', 'Next month'],
+        [29030400, 'months', 2419200],
+        [58060800, 'Last year', 'Next year'],
+        [2903040000, 'years', 29030400],
+        [5806080000, 'Last century', 'Next century'],
+        [58060800000, 'centuries', 2903040000]
+    ];
+    var seconds = (+new Date() - time) / 1000,
+        token = 'ago',
+        list_choice = 1;
+
+    if (seconds == 0) {
+        return 'Just now'
+    }
+    if (seconds < 0) {
+        seconds = Math.abs(seconds);
+        token = 'from now';
+        list_choice = 2;
+    }
+    var i = 0,
+        format;
+    while (format = time_formats[i++])
+        if (seconds < format[0]) {
+            if (typeof format[2] == 'string')
+                return format[list_choice];
+            else
+                return Math.floor(seconds / format[2]) + ' ' + format[1] + ' ' + token;
+        }
+    return time;
+}
+
 function handleMessages(responseText) {	
 	var response = JSON.parse(responseText);
 	pageToken = response.pageToken;
@@ -35,7 +89,7 @@ function handleMessages(responseText) {
 		messageElm.setAttribute("class", "card-holder");
 		messageElm.innerHTML = cardTemplate.replace("{0}", URL_PREFIX + msg.author.photoUrl)
 									.replace("{1}", msg.author.name)
-									.replace("{2}", msg.updated)
+									.replace("{2}", timeAgo(msg.updated))
 									.replace("{3}", msg.content);		
 		messagesHolder.appendChild(messageElm);
 	}
